e3016d4e2dfc01a70ebccfbb7fde188030220f93
Wilson Souza <wilsons80@gmail.com>
2021-11-23 17:18:50 -0300

    Voltando o tamanho da chave do token JWT

--- src/main/java/br/com/cartaoamigo/infra/constantes/SecurityContantes.java
+++ src/main/java/br/com/cartaoamigo/infra/constantes/SecurityContantes.java
@@ -2,7 +2,8 @@ package br.com.cartaoamigo.infra.constantes;
 
 public class SecurityContantes {
 	
-	public static final String API_KEY = "Cartao^Amigo@solucoes@-api@-@caApi2021^duo#cloud$wujdmn@papa&&Cartao-Associados";
+	//public static final String API_KEY = "Cartao^Amigo@solucoes@-api@-@caApi2021^duo#cloud$wujdmn@papa&&Cartao-Associados";
+	public static final String API_KEY = "Cartao^Amigo@solucoes@-api@-@caApi2021";
 	public static final String JWT_PROVIDER = "Bearer";
 	public static final String JWT_ROLE_KEY = "role";
 	public static final String JWT_INVALID_MSG = "Token de Acesso inválido"; 
