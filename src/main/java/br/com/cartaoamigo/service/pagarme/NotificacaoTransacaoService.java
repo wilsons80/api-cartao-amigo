a3b5a0facd759dcc1755e19579c92423e69eeeda
WILSON SOUZA <wilsons80@gmail.com>
2021-12-16 12:36:09 -0300

    Criando o service de webhooks

---
+++ src/main/java/br/com/cartaoamigo/service/pagarme/NotificacaoTransacaoService.java
@@ -0,0 +1,46 @@
+package br.com.cartaoamigo.service.pagarme;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cartaoamigo.cmd.pagseguro.GetNotificacaoTransacaoCmd;
+import br.com.cartaoamigo.cmd.pagseguro.SalvarNotificacaoTransacaoCmd;
+import br.com.cartaoamigo.to.NotificacaoTransacaoTO;
+import br.com.cartaoamigo.ws.pagseguro.to.NotificacaoTransacaoPagSeguroTO;
+
+@RestController
+@RequestMapping(value = "pagarme/notificacao")
+public class NotificacaoTransacaoService {
+	
+	@Autowired private GetNotificacaoTransacaoCmd getCmd;
+	@Autowired private SalvarNotificacaoTransacaoCmd salvarCmd;
+	@Autowired private GetNotificacaoTransacaoCmd getNotificacaoTransacaoCmd;
+	
+	/**
+	 * Endpoint usado para capturar o retorno das transações no Pagseguro.
+	 * @param paramMap
+	 */
+	@PostMapping(path = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public void salvarNotificacao(@RequestParam MultiValueMap<String,String> paramMap) {
+		salvarCmd.salvar(paramMap);
+	}
+	
+	@GetMapping(path = "/{numeroTransacao}", produces = MediaType.APPLICATION_JSON_VALUE )
+	public NotificacaoTransacaoTO findByNumeroTransacao(@PathVariable(name = "numeroTransacao") String numeroTransacao) {
+		return getCmd.findByNumerotransacao(numeroTransacao);
+	}
+	
+	@GetMapping(path = "/consultar/notificacao/{codigoNotificacao}", produces = MediaType.APPLICATION_JSON_VALUE )
+	public NotificacaoTransacaoPagSeguroTO getNotificacacao(@PathVariable(name = "codigoNotificacao") String codigoNotificacao) {
+		 return getNotificacaoTransacaoCmd.getNotificacaoByPagSeguro(codigoNotificacao);
+	}
+	
+	
+}
