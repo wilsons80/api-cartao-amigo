b64c402cd57914b252dd253750f8097f2f707190
WILSON SOUZA <wilsons80@gmail.com>
2022-01-05 15:43:10 -0300

    Serviço de pagamento do PAGAR.ME

--- src/main/java/br/com/cartaoamigo/cmd/gateway/pagarme/recorrencia/GetWebhookPagarmeCmd.java
+++ src/main/java/br/com/cartaoamigo/cmd/gateway/pagarme/recorrencia/GetWebhookPagarmeCmd.java
@@ -1,5 +1,7 @@
 package br.com.cartaoamigo.cmd.gateway.pagarme.recorrencia;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +12,19 @@ import br.com.cartaoamigo.ws.pagarme.webhook.WebhookPagarMeService;
 
 @Component
 public class GetWebhookPagarmeCmd {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(GetWebhookPagarmeCmd.class);
 	@Autowired private WebhookPagarMeService service;
 	
 	public WebHookPagarMeTO getWebhook(String idWebhook) {
 		try {
-			return service.getWebhook(idWebhook);
+			LOGGER.info("Buscando webhook: " + idWebhook);
+			WebHookPagarMeTO webHookPagarMeTO = service.getWebhook(idWebhook);
+			
+			LOGGER.info("Webhook encontrado: " + webHookPagarMeTO.toString());
+			
+			return webHookPagarMeTO;
 		} catch (Exception e) {
+			e.printStackTrace();
 			throw new PagarmeException("Ocorreu um erro ao obter o webhook: " + e.getMessage());
 		}
 	}
